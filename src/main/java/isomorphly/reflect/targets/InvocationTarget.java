5f47d81d57fa261e736e8d8a1dfd5c4333b11bf7
Leonardo Foderaro <startrack79@gmail.com>
2016-03-06 22:18:50 +0100

    first commit

---
+++ src/main/java/isomorphly/reflect/targets/InvocationTarget.java
@@ -0,0 +1,24 @@
+package isomorphly.reflect.targets;
+
+import java.lang.reflect.Method;
+
+public class InvocationTarget {
+
+	private Object instance;
+	
+	private Method method;
+
+	public Object getInstance() {
+		return instance;
+	}
+
+	public Method getMethod() {
+		return method;
+	}
+
+	public InvocationTarget(Object obj, Method method) {
+		this.instance = obj;
+		this.method = method;
+	}
+
+}
