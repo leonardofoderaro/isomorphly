3ae21eb2ccd8f656f488516f8d52851c11e14be3
Leonardo Foderaro <startrack79@gmail.com>
2016-03-06 22:33:22 +0100

    added scanning support for multiple packages

--- src/main/java/isomorphly/reflect/scanners/PackageScanner.java
+++ src/main/java/isomorphly/reflect/scanners/PackageScanner.java
@@ -1,5 +1,27 @@
 package isomorphly.reflect.scanners;
 
+import isomorphly.annotations.Group;
+
+import java.util.Set;
+
+import org.reflections.Reflections;
+
 public class PackageScanner {
+	Reflections reflections;
+	
+	Set<Class<?>> groups;
+	
+	String packageNames[];
+	
+	public PackageScanner(String packageNames[]) {
+		this.packageNames = packageNames;
+	}
+	
+	public void scan() {
+		for (String s : packageNames) {
+		    reflections = new Reflections(s);
+		    groups.addAll(reflections.getTypesAnnotatedWith(Group.class));
+		}
+	}
 
 }
