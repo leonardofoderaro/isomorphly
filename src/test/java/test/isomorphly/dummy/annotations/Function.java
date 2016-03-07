a57693e7144f0dbe1b419f6005fbd39ac0684c86
Leonardo Foderaro <startrack79@gmail.com>
2016-03-07 16:01:42 +0100

    added Runtime RetentionPolicy

--- src/test/java/test/isomorphly/dummy/annotations/Function.java
+++ src/test/java/test/isomorphly/dummy/annotations/Function.java
@@ -1,7 +1,11 @@
 package test.isomorphly.dummy.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import isomorphly.annotations.Component;
 
+@Retention(RetentionPolicy.RUNTIME)
 @Component
 public @interface Function {
 
--- src/test/java/test/isomorphly/dummy/annotations/Plugin.java
+++ src/test/java/test/isomorphly/dummy/annotations/Plugin.java
@@ -2,7 +2,12 @@ package test.isomorphly.dummy.annotations;
 
 import isomorphly.annotations.Group;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
 @Group
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Plugin {
 
 }
