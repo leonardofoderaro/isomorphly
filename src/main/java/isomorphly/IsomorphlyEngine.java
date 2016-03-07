f6dc95e5c5e99612cd03317355b6d9ed90010e21
Leonardo Foderaro <startrack79@gmail.com>
2016-03-07 12:55:41 +0100

    added init method with elements scanning phase (groups and components)

--- src/main/java/isomorphly/IsomorphlyEngine.java
+++ src/main/java/isomorphly/IsomorphlyEngine.java
@@ -1,16 +1,88 @@
 package isomorphly;
 
+import isomorphly.annotations.Component;
+import isomorphly.annotations.Group;
 import isomorphly.reflect.scanners.PackageScanner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.reflections.Reflections;
 
 
 public class IsomorphlyEngine {
 	private PackageScanner packageScanner;
-	
+
+	private Reflections reflections;
+
+	private List<Class<?>> groupAnnotations;
+
+	private List<Class<?>> componentAnnotations;
+
+	private String[] packageNames;
+
+	boolean initialized;
+
 	public IsomorphlyEngine(String packageNames[]) {
-		this.packageScanner = new PackageScanner(packageNames);
+
+		this.initialized = false;
+
+		this.packageScanner = new PackageScanner(this, packageNames);
+
+		this.packageNames = packageNames;
+
+		groupAnnotations = new ArrayList<Class<?>>();
+
+		componentAnnotations = new ArrayList<Class<?>>();
+
 	}
-	
+
 	public void init() {
 		this.packageScanner.scan();
+
+		scanAnnotatedElements();
+
+		this.initialized = true;
 	}
+
+	private void scanAnnotatedElements() {
+		for (String pkgName : this.packageNames) {
+			Reflections reflections = new Reflections(pkgName);
+
+			Set<Class<?>> groups = reflections.getTypesAnnotatedWith(Group.class);
+			groupAnnotations.addAll(groups);
+
+			Set<Class<?>> components = reflections.getTypesAnnotatedWith(Component.class);
+			componentAnnotations.addAll(components);
+		}
+	}
+	
+	/*
+	private void scanAnnotatedComponents() {
+		for (String pkgName : this.packageNames) {
+			Reflections reflections = new Reflections(pkgName);
+			
+			//Set<Method> methods = reflection
+		}
+	}
+	*/
+
+	public final List<Class<?>> getIsomorphlyGroups() {
+		return groupAnnotations;
+	}
+
+	public final List<Class<?>> getIsomorphlyComponents() {
+		return componentAnnotations;
+	}
+
+	public boolean isInitialized() {
+		return this.initialized &&
+				this.groupAnnotations != null &&
+				this.groupAnnotations.size() > 0 &&
+				this.componentAnnotations != null &&
+				this.componentAnnotations.size() > 0;
+	}
+
+
 }
