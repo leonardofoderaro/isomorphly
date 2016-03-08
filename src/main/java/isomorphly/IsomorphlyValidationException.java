a534aae50c5facc59186f7c452f9062c0007c178
Leonardo Foderaro <startrack79@gmail.com>
2016-03-08 09:58:29 +0100

    initial import - Exception triggered when annotated elements validation fails

---
+++ src/main/java/isomorphly/IsomorphlyValidationException.java
@@ -0,0 +1,10 @@
+package isomorphly;
+
+public class IsomorphlyValidationException extends Exception {
+
+    private static final long serialVersionUID = -7556185217858266871L;
+
+    public IsomorphlyValidationException(String message) {
+        super(message);
+    }
+}
