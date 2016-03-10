ae248c7e678512bf3bf2777065b5fdea8ad9f596
Leonardo Foderaro <startrack79@gmail.com>
2016-03-10 21:30:24 +0100

    moved & refactored implementation, from IsomorphlyEngine

--- src/main/java/isomorphly/reflect/scanners/PackageScanner.java
+++ src/main/java/isomorphly/reflect/scanners/PackageScanner.java
@@ -1,24 +1,79 @@
 package isomorphly.reflect.scanners;
 
-import isomorphly.IsomorphlyEngine;
+import isomorphly.IsomorphlyValidationException;
+import isomorphly.annotations.Component;
+import isomorphly.annotations.Group;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.reflections.Reflections;
 
 public class PackageScanner {
-	
-	private String[] packageNames;
-	
-	private IsomorphlyEngine engine;
-	
-	public PackageScanner(IsomorphlyEngine engine, String[] packageNames) {
-		this.engine = engine;
-		this.packageNames = packageNames;
-	}
-	
-	public String[] getPackagesNames() {
-		return this.packageNames;
-	}
-	
-	public IsomorphlyEngine getEngine() {
-		return this.engine;
-	}
-	
+
+  private String[] packageNames;
+
+  private Set<Class<?>> groups;
+
+  private Set<Class<?>> components;
+
+  public PackageScanner(String[] packageNames) throws IsomorphlyValidationException {
+    this.packageNames = packageNames;
+
+    this.groups = new HashSet<>();
+
+    this.components = new HashSet<>();
+
+    loadGroupsDefinitions();
+
+    loadComponentsDefinitions();
+  }
+
+  private void loadGroupsDefinitions() throws IsomorphlyValidationException {
+
+    for (String pkgName : packageNames) {
+
+      Reflections reflections = new Reflections(pkgName);
+
+      Set<Class<?>> foundGroups = reflections.getTypesAnnotatedWith(Group.class);
+      for (Class<?> c : foundGroups) {
+        if (c.isAnnotation()) {
+          groups.add(c);
+        } else {
+          throw new IsomorphlyValidationException("@Group Annotation can be used only on Annotations.");
+        }
+      }
+
+    }
+
+  }
+
+
+  private void loadComponentsDefinitions() throws IsomorphlyValidationException {
+
+    for (String pkgName : packageNames) {
+
+      Reflections reflections = new Reflections(pkgName);
+
+      Set<Class<?>> foundComponents = reflections.getTypesAnnotatedWith(Component.class);
+      for (Class<?> c : foundComponents) {
+        if (c.isAnnotation()) {
+          components.add(c);
+        } else {
+          throw new IsomorphlyValidationException("@Group Annotation can be used only on Annotations.");
+        }
+      }
+
+    }
+
+  }
+
+  public Set<Class<?>> getGroupDefinitions() {
+    return this.groups;
+  }
+
+  public Set<Class<?>> getComponentsDefinitions() {
+    return this.components;
+  }
+
 }
