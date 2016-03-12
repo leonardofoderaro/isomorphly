764c77203829546fcb640e40a2db2a5a8eb79aef
Leonardo Foderaro <startrack79@gmail.com>
2016-03-12 15:26:34 +0100

    added elements for junit tests

---
+++ src/test/java/test/isomorphly/invalid/six/annotations/InvalidGuestPlugin.java
@@ -0,0 +1,11 @@
+package test.isomorphly.invalid.six.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Plugin
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InvalidGuestPlugin {
+
+
+}
